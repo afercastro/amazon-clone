c08dfc3215bbb0d430a04ee2006bb55685734680
Andres Fernandez <afcastro.ssipp@gmail.com>
2020-11-21 20:16:57 -0300

    Add files via upload

---
+++ precache-manifest.7923d70bb231d40b2513c138a788247a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d417bf1814c5cc979c0e5dc14ace3eb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33f39a670b4262b07fb8",
+    "url": "/static/css/main.c53f26aa.chunk.css"
+  },
+  {
+    "revision": "f0680b278d230c56ed30",
+    "url": "/static/js/2.679d29f6.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/static/js/2.679d29f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33f39a670b4262b07fb8",
+    "url": "/static/js/main.a9979c03.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
